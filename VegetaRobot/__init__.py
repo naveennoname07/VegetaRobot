bdc38cc6a9d27a5e5d6f833256cce325ce7e841c
naveennoname07 <85923611+naveennoname07@users.noreply.github.com>
2022-06-02 11:26:41 +0530

    Update __init__.py

--- VegetaRobot/__init__.py
+++ VegetaRobot/__init__.py
@@ -87,9 +87,9 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER',
-    DATABASE_URI = environ.get('DATABASE_URI', "")
-    DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-    COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
     SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
     P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
